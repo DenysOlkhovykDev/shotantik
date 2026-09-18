f1aecdfae4605203a09868d14684c605fd62a1e0
DenysOlkhovykDev <denysolkhovykdev@gmail.com>
2026-09-18 14:34:14 +0300

    186-make-better-shadows

--- src/aircraft/building-parts/background-display.ts
+++ src/aircraft/building-parts/background-display.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics } from "pixi.js";
+import { BlurFilter, Container, Graphics, Sprite } from "pixi.js";
 
 export class BackgroundDisplay extends Container {
   shadowContainer: Container = new Container();
@@ -17,6 +17,57 @@ export class BackgroundDisplay extends Container {
 
   public removeSelectShadow() {
     this.selectShadowContainer.removeChildren();
+  }
+
+  public createShadow(source: Container) {
+    this.shadowContainer.removeChildren();
+
+    const shadow1 = this.cloneContainer(source);
+    const shadow2 = this.cloneContainer(source);
+    const shadow3 = this.cloneContainer(source);
+
+    shadow1.tint = "#000000";
+    shadow1.alpha = 0.4;
+    shadow1.scale = 1.03;
+
+    shadow2.tint = "#000000";
+    shadow2.alpha = 0.2;
+    shadow2.scale = 1.05;
+
+    shadow3.tint = "#000000";
+    shadow3.alpha = 0.1;
+    shadow3.scale = 1.07;
+
+    this.shadowContainer.addChild(shadow1);
+    this.shadowContainer.addChild(shadow2);
+    this.shadowContainer.addChild(shadow3);
+  }
+
+  private cloneContainer(source: Container) {
+    let clone;
+
+    if (source instanceof Sprite) {
+      clone = new Sprite(source.texture);
+    } else if (source instanceof Graphics) {
+      clone = source.clone();
+    } else {
+      clone = new Container();
+    }
+
+    clone.position.copyFrom(source.position);
+    clone.scale.copyFrom(source.scale);
+    clone.pivot.copyFrom(source.pivot);
+    clone.rotation = source.rotation;
+    clone.alpha = source.alpha;
+    clone.visible = source.visible;
+
+    if (source.children && source.children.length > 0) {
+      source.children.forEach((child) => {
+        clone.addChild(this.cloneContainer(child));
+      });
+    }
+
+    return clone;
   }
 
   public createBasicShadow(radius: number) {
